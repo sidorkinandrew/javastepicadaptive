2031469fe10094661be898d07131e7a7a9f88eb0
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2021-01-23 13:19:19 +0000

    task 043

--- src/com/stepik/adaptive/task01_030/Main.java
+++ src/com/stepik/adaptive/task01_030/Main.java
@@ -1,5 +1,10 @@
 package com.stepik.adaptive.task01_030;
+/*
+The interest rate on the deposit is P percent per annum, which are added to the deposit amount at the end of the year.
+The deposit is X dollars and Y cents. Find the deposit amount a year after.
 
+
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
